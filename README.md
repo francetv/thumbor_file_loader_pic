1752323eca82b060ddcc79521732872df9e477ca
btl <bertrand.thill@francetv.fr>
2019-09-06 10:08:07 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # thumbor_file_loader_pic
+Thumbor Fileloader loader
+
+
+```
+LOADER = 'thumbor_fallback_pic_loader.loader' #loader calling. Don't call with fallback
+PIC_LOADER_ROOT_PATH = #root path for file
+
+```
+
+Tested on Debian 9 with:
+- Thumbor (pip) 6.5
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+
+from distutils.core import setup
+
+setup(
+    name = "thumbor_file_loader_pic",
+    packages = ["thumbor_file_loader_pic"],
+    version = "0.1.1",
+    description = "file loader",
+    author = "Bertrand Thill",
+    author_email = "bertrand.thill@francetv.fr",
+    keywords = ["thumbor", "fallback", "images", "nfs"],
+    license = 'MIT',
+    url = 'https://github.com/francetv/thumbor_file_loader_pic',
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Multimedia :: Graphics :: Presentation'
+    ],
+    package_dir = {"thumbor_file_loader_pic": "thumbor_file_loader_pic"},
+    install_requires=['thumbor>=6.5.0'],
+    long_description = """\
+This module test support for file.
+"""
+)
