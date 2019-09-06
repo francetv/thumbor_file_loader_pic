8b6d9e511e35cb646470617a203a3f02bc2a1c83
btl <bertrand.thill@francetv.fr>
2019-09-06 13:19:45 +0200

    npush

--- thumbor_file_loader_pic/file_loader_pic.py
+++ thumbor_file_loader_pic/file_loader_pic.py
@@ -1,6 +1,3 @@
-
-=============== file_loader_pic_fallback.py =================
-  
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -11,20 +8,49 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
-from . import file_loader
-from . import file_loader_pic
+from os import fstat
+from datetime import datetime
+from os.path import join, exists, abspath
+
+from six.moves.urllib.parse import unquote
 from tornado.concurrent import return_future
+
+from thumbor.loaders import LoaderResult
 
 
 @return_future
 def load(context, path, callback):
-    def callback_wrapper(result):
-        if result.successful:
-            callback(result)
-        else:
-            # If file_loader failed try http_loader
-            file_loader.load(context, path, callback)
+    file_path = join(
+        context.config.PIC_LOADER_ROOT_PATH.rstrip('/'), path.lstrip('/'))
+    file_path = abspath(file_path)
+    inside_root_path = file_path.startswith(
+        abspath(context.config.PIC_LOADER_ROOT_PATH))
 
-    # First attempt to load with file_loader
-    file_loader_pic.load(context, path, callback_wrapper)
-    
+    result = LoaderResult()
+
+    if not inside_root_path:
+        result.error = LoaderResult.ERROR_NOT_FOUND
+        result.successful = False
+        callback(result)
+        return
+
+    # keep backwards compatibility, try the actual path first
+    # if not found, unquote it and try again
+    if not exists(file_path):
+        file_path = unquote(file_path)
+
+    if exists(file_path):
+        with open(file_path, 'r') as f:
+            stats = fstat(f.fileno())
+
+            result.successful = True
+            result.buffer = f.read()
+
+            result.metadata.update(
+                size=stats.st_size,
+                updated_at=datetime.utcfromtimestamp(stats.st_mtime))
+    else:
+        result.error = LoaderResult.ERROR_NOT_FOUND
+        result.successful = False
+
+    callback(result)
