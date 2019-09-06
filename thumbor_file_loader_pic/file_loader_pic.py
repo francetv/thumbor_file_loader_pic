30ab70d3ac36b831aee97ef6b1d7a80fda5c087d
btl <bertrand.thill@francetv.fr>
2019-09-06 16:38:36 +0200

    fix

--- thumbor_file_loader_pic/file_loader_pic.py
+++ thumbor_file_loader_pic/file_loader_pic.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -23,21 +24,29 @@ def load(context, path, callback):
     file_path = join(
         context.config.PIC_LOADER_ROOT_PATH.rstrip('/'), path.lstrip('/'))
     file_path = abspath(file_path)
+    file_path_two = join(
+        context.config.FILE_LOADER_ROOT_PATH.rstrip('/'), path.lstrip('/'))
+    file_path_two = abspath(file_path_two)
     inside_root_path = file_path.startswith(
         abspath(context.config.PIC_LOADER_ROOT_PATH))
-
+    inside_root_path_two = file_path_two.startswith(
+        abspath(context.config.FILE_LOADER_ROOT_PATH))
     result = LoaderResult()
 
     if not inside_root_path:
-        result.error = LoaderResult.ERROR_NOT_FOUND
-        result.successful = False
-        callback(result)
+        if not inside_root_path_two:
+          result.error = LoaderResult.ERROR_NOT_FOUND
+          result.successful = False
+          callback(result)
         return
 
     # keep backwards compatibility, try the actual path first
     # if not found, unquote it and try again
     if not exists(file_path):
         file_path = unquote(file_path)
+
+    if not exists(file_path_two):
+        file_path_two = unquote(file_path_two)
 
     if exists(file_path):
         with open(file_path, 'r') as f:
@@ -49,6 +58,18 @@ def load(context, path, callback):
             result.metadata.update(
                 size=stats.st_size,
                 updated_at=datetime.utcfromtimestamp(stats.st_mtime))
+    elif exists(file_path_two):
+         with open(file_path_two, 'r') as f:
+            stats = fstat(f.fileno())
+
+            result.successful = True
+            result.buffer = f.read()
+
+            result.metadata.update(
+                size=stats.st_size,
+                updated_at=datetime.utcfromtimestamp(stats.st_mtime))
+
+
     else:
         result.error = LoaderResult.ERROR_NOT_FOUND
         result.successful = False
